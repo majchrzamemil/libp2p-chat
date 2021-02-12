87abfe7183d903b3f32df1add54bece98740fd44
Emil Majchrzak <majchrzakemil@gitlab.com>
2021-02-12 13:11:17 +0100

    libp2p chat example
    Signed-off-by: Emil Majchrzak <majchrzakemil@gitlab.com>

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+use async_std::{io, task};
+use env_logger::{Builder, Env};
+use futures::prelude::*;
+use libp2p::gossipsub::MessageId;
+use libp2p::gossipsub::{
+    GossipsubEvent, GossipsubMessage, IdentTopic as Topic, MessageAuthenticity, ValidationMode,
+};
+use libp2p::{gossipsub, identity, PeerId};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::time::Duration;
+use std::{
+    task::{Context, Poll},
+};
+
+fn main() {
+    Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    // Create a random PeerId
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {:?}", local_peer_id);
+
+    let transport = libp2p::build_development_transport(local_key.clone()).unwrap();
+
+    let topic = Topic::new("blaze-it");
+
+    let mut swarm = {
+        let message_id_fn = |message: &GossipsubMessage| {
+            let mut s = DefaultHasher::new();
+            message.data.hash(&mut s);
+            MessageId::from(s.finish().to_string())
+        };
+
+        let gossipsub_config = gossipsub::GossipsubConfigBuilder::default()
+            .heartbeat_interval(Duration::from_secs(5))
+            .validation_mode(ValidationMode::Strict)
+            .message_id_fn(message_id_fn)
+            .build()
+            .expect("ok");
+        let mut gossipsub: gossipsub::Gossipsub =
+            gossipsub::Gossipsub::new(MessageAuthenticity::Signed(local_key), gossipsub_config)
+                .expect("Correnct configuration");
+        gossipsub.subscribe(&topic).unwrap();
+
+        if let Some(explicit) = std::env::args().nth(2) {
+            let explicit = explicit.clone();
+            match explicit.parse() {
+                Ok(id) => gossipsub.add_explicit_peer(&id),
+                Err(err) => println!("Failed to parse {}", err),
+            }
+        }
+        libp2p::Swarm::new(transport, gossipsub, local_peer_id)
+    };
+
+    libp2p::Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
+
+    if let Some(to_dial) = std::env::args().nth(1) {
+        let dialing = to_dial.clone();
+        match to_dial.parse() {
+            Ok(to_dial) => match libp2p::Swarm::dial_addr(&mut swarm, to_dial) {
+                Ok(_) => println!("Dialed {:?}", dialing),
+                Err(e) => println!("Dial {:?} failed: {:?}", dialing, e),
+            },
+            Err(err) => println!("Failed to parse address to dial: {:?}", err),
+        }
+    }
+       // Read full lines from stdin
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
+
+    // Kick it off
+    let mut listening = false;
+    task::block_on(future::poll_fn(move |cx: &mut Context<'_>| {
+        loop {
+            if let Err(e) = match stdin.try_poll_next_unpin(cx) {
+                Poll::Ready(Some(line)) => swarm.publish(topic.clone(), line.unwrap().as_bytes()),
+                Poll::Ready(None) => panic!("Stdin closed"),
+                Poll::Pending => break,
+            } {
+                println!("Publish error: {:?}", e);
+            }
+        }
+
+        loop {
+            match swarm.poll_next_unpin(cx) {
+                Poll::Ready(Some(gossip_event)) => match gossip_event {
+                    GossipsubEvent::Message {
+                        propagation_source: peer_id,
+                        message_id: id,
+                        message,
+                    } => println!(
+                        "Got message: {} with id: {} from peer: {:?}",
+                        String::from_utf8_lossy(&message.data),
+                        id,
+                        peer_id
+                    ),
+                    _ => {}
+                },
+                Poll::Ready(None) | Poll::Pending => break,
+            }
+        }
+
+        if !listening {
+            for addr in libp2p::Swarm::listeners(&swarm) {
+                println!("Listening on {:?}", addr);
+                listening = true;
+            }
+        }
+
+        Poll::Pending
+    }))
+}
